c1e45c2f1e40a168457ce63b5b8a35685f08f0ea
Ulises Cadena <ucadena07@gmail.com>
2020-07-10 06:08:16 -0500

    script part1

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $('#mycarousel').carousel({interval: 2000});
+  $('#carouselButton').click(function() {
+      if ($('#carouselButton').children('span').hasClass('fa-pause')){
+           $('#mycarousel').carousel('pause');
+           $('#carouselButton').children('span').removeClass('fa-pause');
+           $('#carouselButton').children('span').addClass('fa-play');
+      }
+      else  if ($('#carouselButton').children('span').hasClass('fa-play')){
+           $('#mycarousel').carousel('cycle');
+           $('#carouselButton').children('span').removeClass('fa-play');
+           $('#carouselButton').children('span').addClass('fa-pause');
+      }  
+  });
+
+   $('#reserveTableButton').click(function() {
+        $('#reservationModal').modal('toggle');
+        $('#reservationModal').modal('show');
+    });
+  
+  $('#loginButton').click(function() {
+      $('#loginModal').modal('toggle');
+      $('#loginModal').modal('show');
+  })
+
+
+});
